0d54827918c4f638b8e728e873851ac1757187d3
Oluwaseun Akogun <seun407@gmail.com>
2019-10-11 12:44:13 +0100

    added initial state and modified entries

--- src/App.js
+++ src/App.js
@@ -33,24 +33,26 @@ const particlesOptions = {
   },
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    entries: 0,
+    joined: new Date()
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        firstname: '',
-        lastname: '',
-        email: '',
-        entries: 0,
-        joined: new Date()
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -59,7 +61,7 @@ class App extends Component {
       firstname: data.firstname,
       lastname: data.lastname,
       email: data.email,
-      entries: 0,
+      entries: data.entries,
       joined: data.joined
     }})
   }
@@ -102,6 +104,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, {entries: count}))
           })
+          .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -110,7 +113,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
